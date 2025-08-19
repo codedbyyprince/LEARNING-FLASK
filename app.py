5b4c697cfb05bdb33d617daabdc69d6d89c9efdc
prince <princenagda35@gmail.com>
2025-08-19 17:55:42 +0530

    today i learnt creting templates and routes and posted form

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    name = request.args.get('name', 'world')        
-    return render_template('index.html', name= name)
+    if request.method == 'POST':
+        return render_template('greet.html', name=request.form.get('name', 'world'))
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
