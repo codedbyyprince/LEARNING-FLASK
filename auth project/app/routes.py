7988102b26699056d8e97be2ce4efab5a8e69c93
prince <princenagda35@gmail.com>
2025-10-30 20:55:51 +0530

    fixed errors and and completed the forms

--- auth project/app/routes.py
+++ auth project/app/routes.py
@@ -1,2 +1,17 @@
-from flask import Flask
+from flask import render_template, redirect, url_for, flash
+from app import app
+from app.form import LoginForm
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.user.data, form.remember_me.data))
+        return redirect(url_for('login'))
+    return render_template('login.html', title='Sign In', form=form)
